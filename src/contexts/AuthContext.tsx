c333f9350c07d45457c018f4754575322cea9aff
igorferreira007 <ferreiraigor007@gmail.com>
2025-05-17 11:38:45 -0300

    Adicionado o token como padrão nas requisições

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
+import { api } from "@/services/api"
 import { createContext, ReactNode, useEffect, useState } from "react"
 
 type AuthContext = {
@@ -20,6 +21,8 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     localStorage.setItem(`${LOCAL_STORAGE_KEY}:user`, JSON.stringify(data.user))
     localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token)
 
+    api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`
+
     setSession(data)
   }
 
@@ -37,6 +40,8 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     const token = localStorage.getItem(`${LOCAL_STORAGE_KEY}:token`)
 
     if (token && user) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+
       setSession({
         token,
         user: JSON.parse(user),
