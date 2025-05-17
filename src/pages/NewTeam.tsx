0c5529bf4e4f0c6cb9c19d9ae278da665398ec0f
igorferreira007 <ferreiraigor007@gmail.com>
2025-05-17 11:40:05 -0300

    Criada a função para cadastrar equipe

--- src/pages/NewTeam.tsx
+++ src/pages/NewTeam.tsx
@@ -3,14 +3,57 @@ import { ButtonText } from "@/components/ButtonText"
 import { Input } from "@/components/Input"
 import { PageTitle } from "@/components/PageTitle"
 import { TextArea } from "@/components/TextArea"
+import { api } from "@/services/api"
+import { AxiosError } from "axios"
+import { FormEvent, useState } from "react"
+import { useNavigate } from "react-router"
+import { z, ZodError } from "zod"
+
+const newTeamSchema = z.object({
+  name: z.string().trim().min(1, "O nome da equipe é obrigatório"),
+  description: z.string(),
+})
 
 export function NewTeam() {
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+
+  const navigate = useNavigate()
+
+  async function onSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    try {
+      const newTeam = newTeamSchema.parse({
+        name,
+        description,
+      })
+
+      await api.post("/teams", newTeam)
+
+      alert("Cadastrado com sucesso!")
+      navigate("/teams")
+    } catch (error) {
+      console.log(error)
+
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message)
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message)
+      }
+
+      alert("Não foi possível cadastrar!")
+    }
+  }
+
   return (
     <>
       <ButtonText />
       <form
         className="flex flex-col gap-6 lg:gap-8 mt-4 lg:mt-8"
-        onSubmit={(e: React.FormEvent) => e.preventDefault()}
+        onSubmit={onSubmit}
       >
         <PageTitle title="Nova equipe" />
 
@@ -18,12 +61,14 @@ export function NewTeam() {
           <Input
             label="Nome"
             placeholder="De um breve nome para a equipe"
+            onChange={(e) => setName(e.target.value)}
             required
           />
         </div>
         <TextArea
           label="Descrição"
           placeholder="Descreva com detalhes a equipe"
+          onChange={(e) => setDescription(e.target.value)}
           required
         />
         <Button size="medium" className="md:self-end" type="submit">
