d491fc501181280a05101fde881fc5cf630a1513
igorferreira007 <ferreiraigor007@gmail.com>
2025-06-08 10:59:56 -0300

    Feito a renderização dos históricos de tarefas na página TaskHistory

--- src/components/LogsTableRow.tsx
+++ src/components/LogsTableRow.tsx
@@ -2,19 +2,19 @@ import { TaskStatus } from "./TaskStatus"
 
 type Props = {
   name: string
-  responsible: string
+  changedBy: string
   status: "pending" | "inProgress" | "completed"
   updatedAt: string
 }
 
-export function LogsTableRow({ name, responsible, status, updatedAt }: Props) {
+export function LogsTableRow({ name, changedBy, status, updatedAt }: Props) {
   return (
     <tr className="bg-background-tertiary h-12">
       <td className="first:rounded-l-lg last:rounded-r-lg first:pl-6 last:pr-6 px-2">
         {name}
       </td>
       <td className="first:rounded-l-lg last:rounded-r-lg first:pl-6 last:pr-6 px-2">
-        {responsible}
+        {changedBy}
       </td>
       <td className="first:rounded-l-lg last:rounded-r-lg first:pl-6 last:pr-6 px-2">
         <TaskStatus status={status} />
--- src/pages/TaskHistory.tsx
+++ src/pages/TaskHistory.tsx
@@ -1,9 +1,42 @@
 import { Input } from "@/components/Input"
 import { LogsTableRow } from "@/components/LogsTableRow"
 import { PageTitle } from "@/components/PageTitle"
+import { useEffect, useState } from "react"
 import { IoIosSearch } from "react-icons/io"
+import { api } from "@/services/api"
+
+type TaskHistory = {
+  id: string
+  taskId: string
+  changedBy: string
+  oldStatus: string
+  newStatus: "pending" | "inProgress" | "completed"
+  changedAt: string
+  task: Task
+  user: User
+}
+
+type Task = {
+  title: string
+}
+
+type User = {
+  name: string
+}
 
 export function TaskHistory() {
+  const [taskHistories, setTaskHistories] = useState<TaskHistory[]>()
+
+  async function fetchTaskHistories() {
+    const { data } = await api.get<TaskHistory[]>("/task-history")
+
+    setTaskHistories(data)
+  }
+
+  useEffect(() => {
+    fetchTaskHistories()
+  }, [])
+
   return (
     <>
       <div className="grid grid-cols-2 grid-rows-2 items-center gap-4 lg:flex lg:justify-between">
@@ -22,7 +55,7 @@ export function TaskHistory() {
                 Tarefa
               </th>
               <th className="font-normal text-start first:rounded-l-lg last:rounded-r-lg first:pl-6 last:pr-6 py-3 px-2">
-                Responsável
+                Alterado por
               </th>
               <th className="font-normal text-start first:rounded-l-lg last:rounded-r-lg first:pl-6 last:pr-6 py-3 px-2">
                 Status
@@ -33,24 +66,17 @@ export function TaskHistory() {
             </tr>
           </thead>
           <tbody>
-            <LogsTableRow
-              name="Formatar computadores da sala de reunião"
-              responsible="Arthur Silva"
-              status="inProgress"
-              updatedAt="16/02/2025"
-            />
-            <LogsTableRow
-              name="Trocar servidor"
-              responsible="João Pedro"
-              status="completed"
-              updatedAt="16/02/2025"
-            />
-            <LogsTableRow
-              name="Instalar Pacote Office"
-              responsible="Arthur Silva"
-              status="pending"
-              updatedAt="16/02/2025"
-            />
+            {taskHistories?.map((taskHistory) => (
+              <LogsTableRow
+                key={taskHistory.id}
+                name={taskHistory.task.title}
+                changedBy={taskHistory.user.name}
+                status={taskHistory.newStatus}
+                updatedAt={new Date(taskHistory.changedAt).toLocaleDateString(
+                  "pt-BR"
+                )}
+              />
+            ))}
           </tbody>
         </table>
       </div>
