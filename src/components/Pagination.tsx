c7612d3b780419293b41864d6daa7f219026dcd8
igorferreira007 <ferreiraigor007@gmail.com>
2025-06-14 12:27:00 -0300

    Ajustado o componente Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,47 +1,36 @@
-import { useState } from "react"
 import { PiCaretLeft, PiCaretRight } from "react-icons/pi"
 import { twMerge } from "tailwind-merge"
 
-export function Pagination() {
-  const [page, setPage] = useState(1)
-  const [totalPage, setTotalPage] = useState(3)
+type Props = {
+  current: number
+  total: number
+  onNext: () => void
+  onPrevious: () => void
+}
 
-  function handlePagination(action: "next" | "previous") {
-    setPage((prevState) => {
-      if (action === "next" && prevState < totalPage) {
-        return prevState + 1
-      }
-
-      if (action === "previous" && prevState > 1) {
-        return prevState - 1
-      }
-
-      return prevState
-    })
-  }
-
+export function Pagination({ current, total, onNext, onPrevious }: Props) {
   return (
     <div className="flex items-center gap-2 w-40 mx-auto mt-4">
       <button
         className={twMerge(
           "bg-background-tertiary w-8 h-8 rounded-lg grid place-content-center text-2xl disabled:cursor-not-allowed! not-disabled:hover:brightness-110 transition",
-          `${page === 1 && "bg-background-tertiary/25"}`
+          `${current === 1 && "bg-background-tertiary/25"}`
         )}
-        onClick={() => handlePagination("previous")}
-        disabled={page === 1}
+        onClick={onPrevious}
+        disabled={current === 1}
       >
         <PiCaretLeft />
       </button>
       <span className="text-sm grow text-center">
-        {page} / {totalPage}
+        {current} / {total}
       </span>
       <button
         className={twMerge(
           "bg-background-tertiary w-8 h-8 rounded-lg grid place-content-center text-2xl disabled:cursor-not-allowed! not-disabled:hover:brightness-110 transition",
-          `${page === totalPage && "bg-background-tertiary/25"}`
+          `${current === total && "bg-background-tertiary/25"}`
         )}
-        onClick={() => handlePagination("next")}
-        disabled={page === totalPage}
+        onClick={onNext}
+        disabled={current === total}
       >
         <PiCaretRight />
       </button>
