934e8d61585778cbc2b4c09a7d8ad46decf75e82
igorferreira007 <ferreiraigor007@gmail.com>
2025-06-15 18:25:48 -0300

    Carregado os dados da tarefa na página TaskDetails

--- src/pages/TaskDetails.tsx
+++ src/pages/TaskDetails.tsx
@@ -2,9 +2,13 @@ import { Button } from "@/components/Button"
 import { ButtonText } from "@/components/ButtonText"
 import { PageTitle } from "@/components/PageTitle"
 import { Select } from "@/components/Select"
-import { useState } from "react"
-import { useNavigate } from "react-router"
+import { api } from "@/services/api"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router"
 import { twMerge } from "tailwind-merge"
+import { Task } from "./Tasks"
+import { AxiosError } from "axios"
+import { ZodError } from "zod"
 
 const variants = {
   status: {
@@ -21,104 +25,167 @@ const variants = {
       color: "bg-status-completed",
     },
   },
+  priority: {
+    low: {
+      title: "Baixa",
+      color: "border-priority-low",
+    },
+    medium: {
+      title: "Média",
+      color: "border-priority-medium",
+    },
+    high: {
+      title: "Alta",
+      color: "border-priority-high",
+    },
+  },
 }
 
 export function TaskDetails() {
+  const [task, setTask] = useState<Task>()
+
   const navigate = useNavigate()
+  const params = useParams()
 
   const [status, setStatus] = useState<"pending" | "inProgress" | "completed">(
     "pending"
   )
+  const [selectValue, setSelectValue] = useState(status)
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value as "pending" | "inProgress" | "completed"
-    setStatus(value)
+  async function handleChangeStatus(
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) {
+    const value = event.target.value as "inProgress" | "completed"
+
+    try {
+      const statusChanged = confirm(
+        "Tem certeza que deseja alterar o status da tarefa?"
+      )
+
+      if (!statusChanged) {
+        setSelectValue(status)
+        return
+      }
+
+      await api.patch(`/tasks/${params.id}/status`, { status: value })
+      setStatus(value)
+      setSelectValue(value)
+    } catch (error) {
+      console.log(error)
+
+      if (error instanceof ZodError) {
+        alert(error.issues[0].message)
+        return
+      }
+
+      if (error instanceof AxiosError) {
+        alert(error.response?.data.message)
+        return
+      }
+    }
   }
 
   function handleEditButtonClick(id: string) {
     navigate(`/edit-task/${id}`)
   }
 
+  useEffect(() => {
+    async function showTask() {
+      const { data } = await api.get<Task>(`/tasks/${params.id}`)
+
+      setTask(data)
+      setStatus(data.status)
+      setSelectValue(data.status)
+    }
+
+    showTask()
+  }, [params.id])
+
   return (
     <>
       <ButtonText />
 
-      <div className="lg:flex items-start justify-between mt-4 lg:mt-8">
-        <div>
-          <PageTitle title="Formatar computadores" />
-          <span className="text-sm font-light">Criado em: 16/02/2025</span>
-        </div>
-        <div className="flex items-center gap-2 max-lg:mt-4">
+      {task && (
+        <>
+          <div className="lg:flex items-start justify-between mt-4 lg:mt-8">
+            <div>
+              <PageTitle title={task.title} />
+              <span className="text-sm font-light">
+                Criado em:{" "}
+                {new Date(task.createdAt).toLocaleDateString("pt-BR")}
+              </span>
+            </div>
+            <div className="flex items-center gap-2 max-lg:mt-4">
+              <span
+                className={twMerge(
+                  "inline-block w-4 h-4 rounded-full",
+                  variants.status[status].color
+                )}
+              />
+              <Select
+                selectSize="small"
+                color="transparent"
+                onChange={handleChangeStatus}
+                value={selectValue}
+              >
+                <option value="pending" disabled>
+                  Pendente
+                </option>
+                <option value="inProgress">Em progresso</option>
+                <option value="completed">Concluído</option>
+              </Select>
+            </div>
+          </div>
+          <p className="text-justify mt-4 lg:mt-8">{task.description}</p>
           <span
             className={twMerge(
-              "inline-block w-4 h-4 rounded-full",
-              variants.status[status].color
+              "inline-block mt-4 border-2 rounded-sm p-2 bg-background-tertiary text-sm",
+              variants.priority[task.priority].color
             )}
-          />
-          <Select
-            selectSize="small"
-            color="transparent"
-            onChange={handleChange}
           >
-            <option value="pending">Pendente</option>
-            <option value="inProgress">Em progresso</option>
-            <option value="completed">Concluído</option>
-          </Select>
-        </div>
-      </div>
-      <p className="text-justify mt-4 lg:mt-8">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-      <span className="inline-block mt-4 border-2 border-priority-medium rounded-sm p-2 bg-background-tertiary text-sm">
-        Média
-      </span>
-      <ul className="flex flex-col gap-2 mt-8 lg:mt-8">
-        <li>
-          <p className="font-light">
-            <span className="font-medium">ID:</span>{" "}
-            e6149176-e649-4c35-86c2-140ebbdd636f
-          </p>
-        </li>
-        <li>
-          <p className="font-light">
-            <span className="font-medium">Equipe:</span> Manutenção de
-            computadores
-          </p>
-        </li>
-        <li>
-          <p className="font-light">
-            <span className="font-medium">Responsável pela tarefa:</span> Arthur
-            Silva
-          </p>
-        </li>
-        <li>
-          <p className="font-light">
-            <span className="font-medium">Última atualização:</span> 17/02/2025
-          </p>
-        </li>
-      </ul>
-      <hr className="border-background-tertiary my-8" />
-      <div className="flex flex-col md:flex-row md:justify-end gap-2 lg:gap-8">
-        <Button color="secondary" size="medium">
-          Excluir tarefa
-        </Button>
-        <Button
-          size="medium"
-          onClick={() =>
-            handleEditButtonClick("307a9dde-54f6-48b8-a5ee-6243882dd97c")
-          }
-        >
-          Editar
-        </Button>
-      </div>
+            {variants.priority[task.priority].title}
+          </span>
+          <ul className="flex flex-col gap-2 mt-8 lg:mt-8">
+            <li>
+              <p className="font-light">
+                <span className="font-medium">ID:</span> {task.id}
+              </p>
+            </li>
+            <li>
+              <p className="font-light">
+                <span className="font-medium">Equipe: </span>
+                {task.team.name}
+              </p>
+            </li>
+            <li>
+              <p className="font-light">
+                <span className="font-medium">Responsável pela tarefa: </span>
+                {task.user ? task.user.name : "Sem responsável"}
+              </p>
+            </li>
+            <li>
+              <p className="font-light">
+                <span className="font-medium">Última atualização: </span>
+                {new Date(task.updatedAt).toLocaleDateString("pt-BR")}
+              </p>
+            </li>
+          </ul>
+          <hr className="border-background-tertiary my-8" />
+          <div className="flex flex-col md:flex-row md:justify-end gap-2 lg:gap-8">
+            <Button color="secondary" size="medium">
+              Excluir tarefa
+            </Button>
+            <Button
+              size="medium"
+              onClick={() =>
+                handleEditButtonClick("307a9dde-54f6-48b8-a5ee-6243882dd97c")
+              }
+            >
+              Editar
+            </Button>
+          </div>
+        </>
+      )}
     </>
   )
 }
